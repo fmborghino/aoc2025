80c0d822dfe8a3e06b0421038cf7cf46a6867bb6
Max Borghino <fmborghino@gmail.com>
2025-11-30 22:19:26 -0800

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,42 @@
 fun main() {
+    val size = 100
+    val target = 0
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var position = 50;
+        var hits = 0;
+        input.forEach {
+            val direction = if (it[0] == 'R') +1 else -1
+            val distance = it.substring(1).toInt()
+            position = (position + (direction * distance)).mod(size)
+//            position = ((position + direction * distance) % 100 + 100) % 100
+            if (position == target) hits += 1
+        }
+        return hits
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var position = 50;
+        var hits = 0;
+        input.forEach { line ->
+            val direction = if (line[0] == 'R') +1 else -1
+            val distance = line.substring(1).toInt()
+            (1..distance).forEach {
+                position = (position + direction).mod(size)
+                if (position == target) hits += 1
+            }
+        }
+        return hits
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput).println()
+    check(part1(testInput) == 3)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+
+    part2(testInput).println()
+    check(part2(testInput) == 6)
     part2(input).println()
 }

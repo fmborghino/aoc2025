073a94e98fecf856852870709670775ee1b20997
Max Borghino <fmborghino@gmail.com>
2025-12-04 00:20:35 -0800

    Template

---
+++ src/Day00.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val day = "00"
+
+    fun part1(input: List<String>): Int {
+        var result = 0
+        return result
+    }
+
+    fun part2(input: List<String>): Int {
+        var result = 0
+        return result
+    }
+
+    val testInput = readInput("Day${day}_test")
+    val input = readInput("Day${day}")
+
+    val t1 = part1(testInput)
+    t1.println()
+    check(t1 == 0)
+
+//    val r1 = part1(input)
+//    r1.println()
+//    check(r1 == 0)
+
+//    val t2 = part2(testInput)
+//    t2.println()
+//    check(t2 == 0)
+
+//    val r2 = part2(input)
+//    r2.println()
+//    check(r2 == 0)
+}
